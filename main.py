15453e7992ae8fa7e49c039132d98c5b901cf42a
Nuri TIRAŞ <nuritiras@gmail.com>
2022-12-11 00:27:14 +0300

    Fonksiyonel yapıldı..

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def get_user_input():
     parse_object.add_option("-m", "--mac", dest="mac_address", help="new mac address")
     return parse_object.parse_args()
 
-def change_mac_address(interface,mac_address)
+def change_mac_address(interface,mac_address):
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", mac_address])
     subprocess.call(["ifconfig", interface, "up"])
