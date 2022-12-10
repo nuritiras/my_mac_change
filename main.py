adccfd494c6d19cedc8b414969e9bac5d3c0dc10
Nuri TIRAŞ <nuritiras@gmail.com>
2022-12-11 00:20:02 +0300

    Fonksiyonel yapıldı.

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import subprocess
 import optparse
 
-parse_object = optparse.OptionParser()
-parse_object.add_option("-i", "--interface", dest="interface", help="interface to change!")
-parse_object.add_option("-m", "--mac", dest="mac_address", help="new mac address")
+def get_user_input():
+    parse_object = optparse.OptionParser()
+    parse_object.add_option("-i", "--interface", dest="interface", help="interface to change!")
+    parse_object.add_option("-m", "--mac", dest="mac_address", help="new mac address")
+    return parse_object.parse_args()
 
-(user_input, arguments) = parse_object.parse_args()
-interface = user_input.interface
-mac_address = user_input.mac_address
+def change_mac_address(interface,mac_address)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", mac_address])
+    subprocess.call(["ifconfig", interface, "up"])
 
 print("MAC Changer Started")
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", mac_address])
-subprocess.call(["ifconfig", interface, "up"])
+(user_input,arguments)=get_user_input()
+change_mac_address(user_input.interface,user_input.mac_address)
