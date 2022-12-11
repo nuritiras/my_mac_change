43a8c75b8431b129bbcbbbcfd4ff809d934cb31b
Nuri TIRAŞ <nuritiras@gmail.com>
2022-12-11 11:04:08 +0300

    Yeni Mac Adresi kontrol fonksiyonu eklendi.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 import optparse
 
@@ -12,6 +13,15 @@ def change_mac_address(interface,mac_address):
     subprocess.call(["ip", "link", "set", "dev", interface, "address", mac_address])
     subprocess.call(["ip", "link", "set", interface, "up"])
 
+def control_new_mac(interface):
+        ip_address_show=subprocess.check_output(["ip", "address", "show", "dev", interface])
+        new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ip_address_show)
+        if new_mac:
+            return new_mac.group(0)
+        else:
+            return None
+
 print("MAC Changer Started")
 (user_input,arguments)=get_user_input()
-change_mac_address(user_input.interface,user_input.mac_address)
+change_mac_address(user_input.interface, user_input.mac_address)
+control_new_mac(user_input.interface)
